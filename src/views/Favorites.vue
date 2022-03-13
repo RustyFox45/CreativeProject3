322b11f154e98397bd26ea6f31882234d06afebd
RustyFox45 <96999279+RustyFox45@users.noreply.github.com>
2022-03-12 19:12:52 -0700

    new stuff

---
+++ src/views/Favorites.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <FavoriteList :favorite="favorite" />
+</div>
+</template>
+
+<script>
+import CartList from "../components/FavoriteList.vue"
+export default {
+  name: 'FavoriteView',
+  components: {
+    FavoriteList
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    cart() {
+      return this.$root.$data.cart;
+    }
+  },
+}
+</script>
