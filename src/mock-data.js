caa40da85e263cf4fa133844277db3c865f38413
RustyFox45 <96999279+RustyFox45@users.noreply.github.com>
2022-03-20 17:59:09 -0600

    Mock Data

--- src/mock-data.js
+++ src/mock-data.js
@@ -1,12 +1,39 @@
 let mock = [
   {
-
+    id: 1,
+    name: "Bulbasaur",
+    region: "Kanto",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
   },
   {
-
+    id: 2,
+    name: "Ivysaur",
+    region: "Kanto",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"
   },
   {
-
+    id: 3,
+    name: "Venusaur",
+    region: "Kanto",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png"
+  },
+  {
+    id: 4,
+    name: "Charmander",
+    region: "Kanto",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
+  },
+  {
+    id: 5,
+    name: "Charmeleon",
+    region: "Kanto",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"
+  },
+  {
+    id: 6,
+    name: "Charizard",
+    region: "Kanto",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"
   },
 ]
 
