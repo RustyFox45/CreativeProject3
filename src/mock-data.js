9710f63879c181376f18956bf174a2a9ebaa51b2
RustyFox45 <96999279+RustyFox45@users.noreply.github.com>
2022-03-23 15:34:54 -0600

    New Pokemon

--- src/mock-data.js
+++ src/mock-data.js
@@ -41,6 +41,48 @@ let mockData = [
     type: "Fire",
     image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"
   },
+  {
+    id: 7,
+    name: "Squirtle",
+    region: "Kanto",
+    type: "Water",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
+  },
+  {
+    id: 8,
+    name: "Wartortle",
+    region: "Kanto",
+    type: "Water",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png"
+  },
+  {
+    id: 9,
+    name: "Blastoise",
+    region: "Kanto",
+    type: "Water",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png"
+  },
+  {
+    id: 10,
+    name: "Caterpie",
+    region: "Kanto",
+    type: "Bug",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png"
+  },
+  {
+    id: 11,
+    name: "Metapod",
+    region: "Kanto",
+    type: "Bug",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/11.png"
+  },
+  {
+    id: 12,
+    name: "Butterfree",
+    region: "Kanto",
+    type: "Bug",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png"
+  },
 ]
 
 export default mockData;
