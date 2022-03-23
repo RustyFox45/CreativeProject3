b58917dfb9f81c759a68935b8b3c1ae6834496de
RustyFox45 <96999279+RustyFox45@users.noreply.github.com>
2022-03-23 15:35:37 -0600

    New

--- src/views/Pokemon.vue
+++ src/views/Pokemon.vue
@@ -4,6 +4,8 @@
     <ul class="pure-menu-list">
       <li class="pure-menu-item"><a @click="select('Fire')" href="#" class="pure-menu-link">Fire</a></li>
       <li class="pure-menu-item"><a @click="select('Leaf')" href="#" class="pure-menu-link">Leaf</a></li>
+      <li class="pure-menu-item"><a @click="select('Water')" href="#" class="pure-menu-link">Water</a></li>
+      <li class="pure-menu-item"><a @click="select('Bug')" href="#" class="pure-menu-link">Bug</a></li>
     </ul>
   </div>
   <PokemonList :pokemons="pokemons" />
